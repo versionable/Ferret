b0a94c26dfb6d736b4baab1a06bbff2be6e85905
Harry Walter <harry.walter@lqdinternet.com>
2011-04-11 12:30:21 +0100

    Fix fileinfo to work for php5.3+

--- src/Ferret/Detector/Fileinfo.php
+++ src/Ferret/Detector/Fileinfo.php
@@ -12,7 +12,7 @@ class Fileinfo extends DetectorAbstract
   
   public function __construct()
   {
-    if (!class_exists('\finfo_file'))
+    if (!class_exists('finfo'))
     {
       throw new DetectorException('Fileinfo extension is not loaded');
     }
@@ -31,16 +31,21 @@ class Fileinfo extends DetectorAbstract
   public function detect($filepath)
   {
     // Support for PHP 5.3+
-    if (defined(FILEINFO_MIME_TYPE))
+    if (defined('FILEINFO_MIME_TYPE'))
     {
       $finfo = new \finfo(FILEINFO_MIME_TYPE);
+
+      $type = $finfo->file($filepath);
     }
     else
     {
-      $finfo = \finfo(FILEINFO_MIME);
+      $finfo = \finfo_open(FILEINFO_MIME);
+
+      $type = \finfo_file($finfo, $filepath);
+
+      \finfo_close($finfo);
     }
     
-    $type = $finfo->file($finfo, $filepath);
     
     if ($type !== false)
     {
